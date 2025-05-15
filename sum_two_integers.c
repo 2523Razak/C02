0f0d3ce8589c27e99898b88ba3db3645e7fd058c
2523Razak <68425641+2523Razak@users.noreply.github.com>
2025-05-16 00:56:15 +0100

    Update sum_two_integers.c

--- sum_two_integers.c
+++ sum_two_integers.c
@@ -1,13 +1,13 @@
 #include <stdio.h> // Pour scanf et printf
 
 int main() {
-    int a, b; // Déclaration des deux entiers
+    int v1, v2; // Déclaration des deux entiers
 
     // Lire deux entiers saisis sur la même ligne
-    scanf("%d %d", &a, &b);
+    scanf("%d %d", &v1, &v2);
 
     // Afficher leur somme
-    printf("La somme est : %d\n", a + b);
+    printf("La somme est : %d\n", v1 + v2);
 
     return 0; // Fin du programme
 }
