91b386717e5285d8e39cd13330c5f08deb6bf888
2523Razak <68425641+2523Razak@users.noreply.github.com>
2025-05-16 07:38:52 +0100

    Update even_or_odd.c

--- even_or_odd.c
+++ even_or_odd.c
@@ -1,14 +1,10 @@
 #include <stdio.h> // pour printf et scanf
-
+int n;      // pour stocker le nombre
 int main() {
-    int n;      // pour stocker le nombre
-    int reste;  // pour stocker le reste de la division par 2
     scanf("%d", &n);       // on lit un nombre
-    reste = n % 2;         // on calcule le reste de n divisé par 2
-    if (reste == 0) {
+    if ((n % 2) == 0) {
         printf("%d est un nombre pair.\n", n);
-    }
-    if (reste != 0) {
+    }else{
         printf("%d est un nombre impair.\n", n);
     }
     return 0; // fin du programme
