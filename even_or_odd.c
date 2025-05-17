b31125a7f71e54e2b9f338a296bc84b03a7f845c
2523Razak <68425641+2523Razak@users.noreply.github.com>
2025-05-18 00:52:36 +0100

    Update even_or_odd.c

--- even_or_odd.c
+++ even_or_odd.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 int main() {
-    int nombre;
-    scanf("%d", &nombre);
+    int n = 0;
+    scanf("%d", &n);
     if (n % 2 == 0) {
-        printf("%d est un nombre pair.\n", nombre);
+        printf("%d est un nombre pair.\n",n);
     } else {
-        printf("%d est un nombre impair.\n", nombre);
+        printf("%d est un nombre impair.\n",n);
     }
     return 0;
 }
