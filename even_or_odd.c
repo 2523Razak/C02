3ac09c277cc9c0a1a8d5d6510bbbd8516a4afe9f
2523Razak <68425641+2523Razak@users.noreply.github.com>
2025-05-18 00:34:35 +0100

    Update even_or_odd.c

--- even_or_odd.c
+++ even_or_odd.c
@@ -1,20 +1,15 @@
 #include <stdio.h>  // pour printf et scanf
 
 int main() {
-    int nombre;  // déclaration d'une variable pour stocker le nombre entré
-
-    // Demander à l'utilisateur d'entrer un entier
-    printf("Entrez un nombre entier : ");
-    scanf("%d", &nombre);  // lecture du nombre entier
-
+    int normbre
+    scanf("%d",&nombre)
     // Vérification avec l'opérateur modulo
     if (nombre % 2 == 0) {
         // Si le reste de la division par 2 est 0, alors c'est pair
-        printf("%d est un nombre pair.\n", nombre);
+        printf("%d est un nombre pair.", nombre);
     } else {
         // Sinon, c'est impair
-        printf("%d est un nombre impair.\n", nombre);
+        printf("%d est un nombre impair.", nombre);
     }
-
     return 0;  // Fin du programme
 }
